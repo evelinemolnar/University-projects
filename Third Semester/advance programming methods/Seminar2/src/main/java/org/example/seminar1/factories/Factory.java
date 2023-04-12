69c5fc5a22759c3327c90f2c5b4581a11843507c
evelinemolnar <noemi.eveline.molnar@stud.ubbcluj.ro>
2023-04-13 01:20:26 +0300

    .

--- Third Semester/advance programming methods/Seminar2/src/main/java/org/example/seminar1/factories/Factory.java
+++
@@ -1,7 +0,0 @@
-package org.example.seminar1.factories;
-
-import org.example.seminar1.containers.Container;
-
-public interface Factory {
-    Container createContainer(Strategy strategy);
-}
--- Third Semester/advance programming methods/Seminar2/src/main/java/org/example/seminar1/factories/Strategy.java
+++
@@ -1,6 +0,0 @@
-package org.example.seminar1.factories;
-
-public enum Strategy {
-    FIFO,
-    LIFO,
-}
